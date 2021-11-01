50874f384e81cf961be317c541005ef5aca7cf30
David Howes <dthowes@umich.edu>
2021-10-31 22:31:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 // Modules
 module "ec2_minecraft" {
   source  = "app.terraform.io/Howes/ec2-minecraft/aws"
-  version = "1.0.2"
+  version = "1.0.3"
 
   environment_tag = "Prod"
   sg_id = "${module.sg_minecraft.sg_id}"
